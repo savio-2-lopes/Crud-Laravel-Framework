820bf54e68421be7c20be998769d8666c9c33900
savio-2-lopes <savioaugulopes@gmail.com>
2022-09-04 11:44:40 -0300

    Apply Migration Employees

---
+++ app/database/migrations/2022_09_04_141821_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100);
+            $table->char('cpf', 11);
+            $table->date('data_contratacao');
+            $table->date('data_demisao')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
